45eeb023edb89bfc2a7d7662324dc94ce1d8fcf7
tluu5 <tluu5@uic.edu>
2022-02-14 23:17:40 -0600

    changes

--- flask/fundamentals/hello_flask/app1.py
+++ flask/fundamentals/hello_flask/app1.py
@@ -1,7 +1,7 @@
-from flask import Flask, hello_flask
+from flask import Flask, render_template
 app = Flask(__name__)
 @app.route('/')
 def index():
-    return hello_flask('app1_index.html')
+    return render_template('index.html')
 if __name__ == "__main__":
     app.run(debug=True)
--- fundamentals/extras/Hangman.py
+++ fundamentals/extras/Hangman.py
@@ -11,9 +11,9 @@ def hangman():
     word_letters = set(word)
     alphabet = set(string.ascii_uppercase)
     used_letters = set()
-    lives = 6
+    lives = 7
     while len(word_letters) > 0 and lives > 0:
-        print('You have', lives, 'lives left and You have used these letters: ', ' '.join(used_letters))
+        print('You have', lives, 'lives left and you have used these letters: ', ' '.join(used_letters))
         word_list = [letter if letter in used_letters else '-' for letter in word]
         print('Current word: ', ' '.join(word_list))
         user_letter = input('Guess a letter: ').upper()
@@ -21,14 +21,17 @@ def hangman():
             used_letters.add(user_letter)
             if user_letter in word_letters:
                 word_letters.remove(user_letter)
+                print('')
             else:
                 lives = lives - 1 
-                print('Letter is not in word.')
+                print('\nLetter, ', user_letter, 'is not in word.')
         elif user_letter in used_letters:
-            print('You have already used that character. Please try again.')
+            print('\nYou have already used that character. Please try again.')
         else:
-            print('Invalid character. PLease try again.')
+            print('\nInvalid character. PLease try again.')
     if lives == 0:
         print('You died, sorry. The word was', word)
     else:
         print('You guessed the word', word, '!!')
+if __name__ == '__main__':
+    hangman()
